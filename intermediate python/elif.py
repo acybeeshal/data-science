5dd041af55bb851b5a720f18fb6beeba83c8baf1
acybeeshal <acybeeshal@gmail.com>
2025-09-26 08:32:34 +0545

    adding the elif

--- intermediate python/elif.py
+++ intermediate python/elif.py
@@ -2,14 +2,19 @@
 room = "kit"
 area = 14.0
 
-# if-else construct for room
+
+# if-elif-else construct for room
 if room == "kit" :
     print("looking around in the kitchen.")
+elif room == "bed":
+    print("looking around in the bedroom.")
 else :
     print("looking around elsewhere.")
 
-# if-else construct for area
+# if-elif-else construct for area
 if area > 15 :
     print("big place!")
+elif area > 10 :
+    print("medium size, nice!")
 else :
     print("pretty small.")
